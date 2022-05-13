5ee8e75f15a6ec4db9cc6bd3c82acd033ac4d01d
James Tan <james@mercstudio.com>
2022-05-13 17:24:45 +0800

    removed footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <main class="py-20 px-10 text-center">
-    <slot />
-    <Footer />
+    <NuxtPage />
     <div class="mt-5 mx-auto text-center opacity-25 text-sm">
       [Default Layout]
     </div>
